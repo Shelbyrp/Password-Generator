821854d3ec65a060b5defe124ed3300e62156d3a
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-03-21 11:37:34 +0800

    updates to comments and README

--- README.md
+++ README.md
@@ -6,23 +6,30 @@ Additions to the Javascript code allows for the prompts for selection and based 
 
 Criteria that the user will be prompted to select when they have clicked on the generate password button is as follows:
 
-1. The length of their password so long as it is a minimum of 8 characters and a maximum of 128.
+1. The length of their password. Must be a minimum of 8 characters and a maximum of 128.
 
-![](./assets/images/screenshot2.png)
+![first criteria](./assets/images/screenshot2.png)
 
 2. User is then prompted to select if they would like to include numbers in their password.
 
+![second criteria](./assets/images/screenshot3.png)
+
 3. User is then prompted to select if they would like to include special characters in their password.
+
+![third criteria](./assets/images/screenshot4.png)
 
 4. User is then prompted to select if they would like to include uppercase letters in their password.
 
+![fourth criteria](./assets/images/screenshot5.png)
+
 5. User is then prompted to select if they would like to include lowercase letters in their password.
 
-Note: if no length of password is set, the user will be prompted to enter a value or else a password will not be generated.
+![fifth criteria](./assets/images/screenshot6.png)
+
+Note: if an invalid length of a password is entered, the user will be prompted to enter a valid value or else a password will not be generated. If the user cancels before entering a length, they will be notified that they exited in the application.
 
 Once all criteria is confirmed, a password in generated and displayed on the page for the user to utilse.
 
 
 Final URL: https://shelbyrp.github.io/Password-Generator/
 
-![](./assets/images/03-javascript-homework-demo.png)
--- script.js
+++ script.js
@@ -1,100 +1,124 @@
-//TO DO
-// When a user clicks on the generate password button
-// Ask user with a prompt the length of password. Minimum 8 characters and a max of 128. Input must be entered.
-
-// User input variables: 
+// User input variables 
 var length;
 var confirmNumber;
 var confirmCharacter;
 var confirmUppercase;
 var confirmLowercase;
 
-// Selection criteria Arrays: 
+// Selection criteria Arrays
 character = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"];
 number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 Lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 Uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Choices declared outside the if statement so they can be concatenated upon condition
+// Based on user input, generate choices as an array for random selection
 var choices;
 
-// Start function to generate password
+// Function to generate password
 function generatePassword() {
     // Asks for user input and verify
     length = 0;
-    while (!length || length < 8 || length > 128) {
-        length = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128"));
-        if (length < 8 || length > 128) {
+        //-------------------------------------------------------------------------------
+    // Enter loop until valid number selected
+    while (length < 8 || length > 128) {
+        length = parseInt(prompt("How many characters would you like your password? Choose between 8 and 128")) ;
+        // Exit loop if user selects cancel or zero
+        if (!length === true || length === 0) {
+            console.log(!length, length);
+            return ("You exited the application");
+        }
+        // Alert user if selection less than eight or greater than one hundred and twenty eight
+        else if (length < 8 || length > 128) {
             alert("You must choose between 8 and 128");
         }
     }
-    // Verify user input for criteria
+    //-------------------------------------------------------------------------------
+    //Set confirm variables to false because following 'if statements' require true results
     confirmNumber = false;
     confirmCharacter = false;
     confirmUppercase = false;
     confirmLowercase = false;
+    //-------------------------------------------------------------------------------
+     // Verify user input for criteria
     while (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
         confirmNumber = confirm("Do you wish to include numbers?");
         confirmCharacter = confirm("Do you wish to include special characters?");
         confirmUppercase = confirm("Do you wish to include uppercase letters?");
         confirmLowercase = confirm("Do you wish to include lowercase letters?");
+         // Alert user if no selection is true
         if (!confirmCharacter && !confirmNumber && !confirmUppercase && !confirmLowercase) {
             alert("You must choose atleast one criteria!");
         }
     }
-
-    // First if statement that uses user input prompts to determine choices
-    // Else if for 4 positive options
+    //-------------------------------------------------------------------------------
+    // Selection criteria matrix (number, character, uppercase, lowercase)
+    // First 'if statement' that uses user input prompts to determine choices
+    //-------------------------------------------------------------------------------
+    // If for 4 positive options (true, true, true, true)
     if (confirmCharacter && confirmNumber && confirmUppercase && confirmLowercase) {
         choices = character.concat(number, Lowercase, Uppercase);
     }
-    // Else if for 3 positive options
-    else if (confirmCharacter && confirmNumber && confirmUppercase) {
-        choices = character.concat(number, Uppercase);
-    }
-    else if (confirmCharacter && confirmNumber && confirmLowercase) {
-        choices = character.concat(number, Lowercase);
-    }
+    //-------------------------------------------------------------------------------
+    // Else if for 3 positive options (false, true, true, true)
     else if (confirmCharacter && confirmLowercase && confirmUppercase) {
         choices = character.concat(Lowercase, Uppercase);
     }
+    // Else if for 3 positive options (true, false, true, true)
     else if (confirmNumber && confirmLowercase && confirmUppercase) {
         choices = number.concat(Lowercase, Uppercase);
+    }    
+    // Else if for 3 positive options (true, true, false, true)
+    else if (confirmCharacter && confirmNumber && confirmLowercase) {
+        choices = character.concat(number, Lowercase);
     }
-    // Else if for 2 positive options 
+    // Else if for 3 positive options (true, true, true, false)
+    else if (confirmCharacter && confirmNumber && confirmUppercase) {
+        choices = character.concat(number, Uppercase);
+    }
+    //-------------------------------------------------------------------------------
+    // Else if for 2 positive options (true, true, false, false)
     else if (confirmCharacter && confirmNumber) {
         choices = character.concat(number);
-
-    } else if (confirmCharacter && confirmLowercase) {
-        choices = character.concat(Lowercase);
-
-    } else if (confirmCharacter && confirmUppercase) {
-        choices = character.concat(Uppercase);
-    }
+    } 
+    // Else if for 2 positive options (true, false, true, false)
+    else if (confirmNumber && confirmUppercase) {
+        choices = number.concat(Uppercase);
+    }        
+    // Else if for 2 positive options (true, false, false, true) 
     else if (confirmLowercase && confirmNumber) {
         choices = Lowercase.concat(number);
-
-    } else if (confirmLowercase && confirmUppercase) {
+    }
+    // Else if for 2 positive options (false, false, true, true)
+    else if (confirmLowercase && confirmUppercase) {
         choices = Lowercase.concat(Uppercase);
-
-    } else if (confirmNumber && confirmUppercase) {
-        choices = number.concat(Uppercase);
-    }
-    // Else if for 1 positive option
-    else if (confirmCharacter) {
-        choices = character;
-    }
+    }    
+    // Else if for 2 positive options (false, true, true, false) 
+    else if (confirmCharacter && confirmUppercase) {
+        choices = character.concat(Uppercase);   
+    } 
+    // Else if for 2 positive options (false, true, false, true)  
+    else if (confirmCharacter && confirmLowercase) {
+        choices = character.concat(Lowercase);
+    }     
+    //-------------------------------------------------------------------------------    
+    // Else if for 1 positive option (true, false, false, false)
     else if (confirmNumber) {
         choices = number;
     }
+    // Else if for 1 positive option (false, true, false, false)
+    else if (confirmCharacter) {
+        choices = character;
+    }
+    //Else if for 1 positive option (false, false, true, false)
+    else if (confirmUppercase) {
+        choices = space.concat(Uppercase);
+    }
+    // Else if for 1 positive option (false, false, false, true)
     else if (confirmLowercase) {
         choices = Lowercase;
     }
-    // Created space variable to fill uppercase conversion
-    else if (confirmUppercase) {
-        choices = space.concat(Uppercase);
-    };
-
+    ;
+    //-------------------------------------------------------------------------------
     // password variable is an array placeholder for user generated amount of length
     var password = [];
 
@@ -105,13 +129,12 @@ function generatePassword() {
         password.push(pickChoices);
     }
     // This joins the password array and converts it to a string
-    // Worked with a tutor to incorporate this option
     var ps = password.join("");
     UserInput(ps);
     return ps;
 }
-
-// This puts the password value into the textbox
+//-------------------------------------------------------------------------------
+// Enters the password value into the textbox
 function UserInput(ps) {
     document.getElementById("password").textContent = ps;
 }
